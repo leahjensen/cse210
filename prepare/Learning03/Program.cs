f413fb1801a9b9ab479d87979b70704268d3d8ed
Leah Jensen <leahjensen8@gmail.com>
2025-02-05 16:16:41 -0700

    w05 prepare unit 3

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,43 @@
 using System;
 
+class BankAccount
+{
+    private double balance;
+
+    public BankAccount(double initialBalance)
+    {
+        balance = initialBalance;
+    }
+
+    public void Deposit(double amount)
+    {
+        if (amount > 0)
+        {
+            balance += amount;
+        }
+    }
+
+    public void Withdraw(double amount)
+    {
+        if (amount > 0 && amount <= balance)
+        {
+            balance -= amount;
+        }
+    }
+
+    public double GetBalance()
+    {
+        return balance;
+    }
+}
+
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Learning03 World!");
+        BankAccount account = new BankAccount(1000);
+        account.Deposit(500);
+        account.Withdraw(200);
+        Console.WriteLine("Balance: " + account.GetBalance());
     }
 }
